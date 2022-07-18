ee386d9f9ee0d3c4850960e597771ef44c6b0c3e
FranciscoGranados1994 <50217315+FranciscoGranados1994@users.noreply.github.com>
2022-07-18 12:43:01 -0500

    Feature/integrate lighthouse ci (#6)
    * integrate Lighthouse into Github actions workflow

---
+++ lighthouserc.js
@@ -0,0 +1,22 @@
+// Reference: https://web.dev/lighthouse-ci/
+/* eslint-disable */
+module.exports = {
+  ci: {
+    collect: {
+      startServerCommand: 'npm run start',
+      url: ['http://localhost:3000'],
+      numberOfRuns: 1,
+    },
+    assert: {
+      assertions: {
+        'categories:performance': ['error', { minScore: 0.9 }],
+        'categories:accessibility': ['error', { minScore: 0.9 }],
+        'categories:best-practices': ['error', { minScore: 0.9 }],
+        'categories:seo': ['error', { minScore: 0.9 }],
+      },
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+}
